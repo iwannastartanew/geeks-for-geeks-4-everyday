24b265deb73404d154809e405b5e3653596392ab
Vohn Daxen Sobresida <sobresidavohndaxen@gmail.com>
2024-07-06 23:59:51 +0800

    July 6, 2024

--- Indexes of Subarray Sum.cpp
+++ Indexes of Subarray Sum.cpp
@@ -1,40 +1,23 @@
 class Solution {
   public:
-    // Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(vector<int> arr, int n, long long s) {
-        // Your code here
         long long sum=0;
-        vector<int> v;
-        int l=1,r=0,flag=0; 
+        int j=0;
         for(int i=0;i<n;i++){
-            sum=sum+arr[i];
-            if(sum==s){
-                r++;
-                flag=1;
-                break;
-            }
-            else if(sum<s){
-                r++;
-                continue;
-            }
-            else{
-                sum=sum-arr[l-1];
-                r++;
-                l++;
+            sum+=(long long)arr[i];
+            if(sum>=s){
+                
+                while(sum>s && j<i){
+                    sum-=arr[j];
+                    j++;
+                }
+                
                 if(sum==s){
-                    break;
+                    return {j+1,i+1};
                 }
             }
         }
-        if(((r==0)||(flag=0))||(s==0)){
-            v.push_back(-1);
-            return v;
-        }
-        else{
-            v.push_back(l);
-            v.push_back(r);
-            return v;
-        }
         
+       return  {-1};
     }
 };
